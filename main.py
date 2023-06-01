f26de238223d8f4c93a9e1903e200b9d781a0c01
MixFix7 <126948322+MixFix7@users.noreply.github.com>
2023-06-01 11:21:45 +0300

    add gpt bot

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import openai
+from key import openai_key
+
+
+openai.api_key = openai_key
+
+
+def gpt_bot(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        promt=message,
+        temperature=0.25,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+    )
+    ai_answer = response['choices'][0]['text']
+    return ai_answer
+
+
 
 
 
