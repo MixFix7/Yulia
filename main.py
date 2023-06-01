ec70a1adca797b33564af68ca9213bcf387821f7
MixFix7 <126948322+MixFix7@users.noreply.github.com>
2023-06-01 16:05:12 +0300

    add memory

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import openai
 from key import openai_key
 
-
 openai.api_key = openai_key
 
 
-def gpt_bot(message):
+def Yulia_Bot(history):
     response = openai.Completion.create(
         model="text-davinci-003",
-        promt=message,
+        prompt=history,
         temperature=0.25,
         max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.5,
         presence_penalty=0.0,
+        stop=[" Lubomyr:", " Yulia:"]
     )
     ai_answer = response['choices'][0]['text']
     return ai_answer
 
 
+while True:
+    user_message = input("> ")
 
+    if user_message == "exit":
+        break
 
+    with open("memory.txt", 'a') as f:
+        f.write(f"\nLubomyr: {user_message}")
 
+    with open("memory.txt", 'r') as f:
+        memory = f.read()
+
+    Yulia_message = Yulia_Bot(memory)
+
+    with open("memory.txt", 'a') as f:
+        f.write(f"\n{Yulia_message}")
+
+    print(f"\n{Yulia_message}")
 
