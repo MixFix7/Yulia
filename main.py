7d061a322c260f570b5bb7d0e63810aa49ad78f2
MixFix7 <126948322+MixFix7@users.noreply.github.com>
2023-06-02 10:12:26 +0300

    create db

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import openai
+import sqlite3
 from key import openai_key
+from gpytranslate import SyncTranslator
+
+t = SyncTranslator()
+
+conn = sqlite3.connect('yulia.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS ChatWithYulia
+                (id INTEGER PRIMARY KEY, 
+                name TEXT NOT NULL, 
+                message TEXT NOT NULL)''')
 
 openai.api_key = openai_key
 
@@ -19,10 +32,16 @@ def Yulia_Bot(history):
     return ai_answer
 
 
+
+
+
 while True:
     user_message = input("> ")
 
+    cursor.execute("INSERT INTO ChatWithYulia (name, message) VALUES (?, ?)", ('Lubomyr', user_message))
+
     if user_message == "exit":
+        conn.close()
         break
 
     with open("memory.txt", 'a') as f:
@@ -32,6 +51,11 @@ while True:
         memory = f.read()
 
     Yulia_message = Yulia_Bot(memory)
+
+    cursor.execute("INSERT INTO ChatWithYulia (name, message) VALUES (?, ?)", ('Yulia', Yulia_message))
+
+    conn.commit()
+
 
     with open("memory.txt", 'a') as f:
         f.write(f"\n{Yulia_message}")
