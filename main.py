e6769d702e157970106b240c65913cb39bbec23f
MixFix7 <mashtalar95@gmail.com>
2023-06-05 21:57:30 +0300

    update database

--- main.py
+++ main.py
@@ -19,44 +19,39 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS ChatWithYulia
 openai.api_key = openai_key
 
 
-def Yulia_Bot2(prompt):
-    url = f"https://api.openai.com/v1/models/text-davinci-003/{openai_key}/"
-    message = requests.request("POST", url)
-
 def Yulia_Bot(prompt):
-
-    chat_history = [
-        {'role': "system", "content": description_yulia},
-        {'role': "user", "content": ""},
-        {'role': "Yulia", "content": prompt}
-    ]
-
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=chat_history,
+        prompt=prompt,
         temperature=0.25,
         max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.5,
         presence_penalty=0.0,
-        stop=[ "Lubomyr:", " Yulia:"]
+        stop=["Lubomyr:", "Yulia:"]
     )
-    ai_answer = response['choices'][0]['text']
-    return ai_answer
+    return response["choices"][0]["text"]
 
 
 
 
 
 while True:
-    user_message = input("> ")
 
-    cursor.execute("INSERT INTO ChatWithYulia (name, message) VALUES (?, ?)", ('Lubomyr', user_message))
+    user_message = input("> ")
 
     if user_message == "exit()":
         conn.close()
         break
+
+    cursor.execute("INSERT INTO ChatWithYulia (name, message) VALUES (?, ?)", ('Lubomyr', user_message))
+
+    all_messages_human = cursor.execute("SELECT * FROM ChatWithYulia WHERE name = 'Lubomyr' ")
+    all_messages_Yulia = cursor.execute("SELECT * FROM ChatWithYulia WHERE name = 'Yulia' ")
+
+    print(all_messages_human.description, all_messages_Yulia)
+
 
     with open("memory.txt", 'a') as f:
         f.write(f"\nLubomyr: {user_message}")
@@ -70,9 +65,7 @@ while True:
 
     conn.commit()
 
-
     with open("memory.txt", 'a') as f:
-        f.write(f"\n{Yulia_message}")
+         f.write(f"\n{Yulia_message}")
 
     print(f"\n{Yulia_message}")
-
